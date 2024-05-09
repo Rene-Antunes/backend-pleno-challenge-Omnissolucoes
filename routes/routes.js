3bad6a063ae8b6c901eaa2fc6474ce959c20bf7c
Renê Antunes Ladeira <93138911+Rene-Antunes@users.noreply.github.com>
2024-05-09 15:47:05 -0300

    remove: remove websocketController.js and depence ws

--- routes/routes.js
+++ routes/routes.js
@@ -3,22 +3,12 @@ const router = express.Router()
 const authController = require("../Controllers/loginAuth.js")
 const userController = require('../Controllers/userController.js')
 const authMiddleware = require("../Middleware/tokenAuth.js")
-const { wss, broadcastMessage }= require("../Controllers/websocketController.js")
 
 router.post('/login', authController.loginHandler)
 router.post(
   '/register',
  userController.userRegister
 )
-
-router.post("/messages", (req, res)=>{
-  const {text} = req.body
-
-  broadcastMessage({text})
-
-  res.status(201)
-  .json({ message: 'Message sent successfully', text });
-})
 
 router.get(
   '/users',
